ea0f8d58b5e5abb7909f55dbf0ff6bbcf49cb38f
aumkarringe <aumkarringe@gmail.com>
2025-10-15 15:28:00 -0700

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Add basename for GitHub Pages */}
+      <BrowserRouter basename="/MedicineMatch/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
